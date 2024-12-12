6345a613e6df2b6e76f3e0e8a462248dfcc72b54
dayeee5 <dayeee5@naver.com>
2024-12-13 03:50:03 +0900

    Update db.js authMiddlewares.js app.js

--- src/app.js
+++ src/app.js
@@ -2,23 +2,32 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-const userRoutes = require('./routes/userRoutes');
-const todoRoutes = require('./routes/todoRoutes');
-const categRoutes = require('./routes/categRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const qnaRoutes = require('./routes/qnaRoutes');
+/* 관리자 view 설정 */
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// const userRoutes = require('./routes/userRoutes');
+// const todoRoutes = require('./routes/todoRoutes');
+// const categRoutes = require('./routes/categRoutes');
+// const chatRoutes = require('./routes/chatRoutes');
+// const qnaRoutes = require('./routes/qnaRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 /* 미들웨어 설정 */
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
 
 /* 라우트 연결 */
-app.use('/user', userRoutes);
-app.use('/todo', todoRoutes);
-app.use('/categ', categRoutes);
-app.use('/chat', chatRoutes);
-app.use('/qna', qnaRoutes);
+// app.use('/user', userRoutes);
+// app.use('/todo', todoRoutes);
+// app.use('/categ', categRoutes);
+// app.use('/chat', chatRoutes);
+// app.use('/qna', qnaRoutes);
+app.use('/admin', adminRoutes);
 
 module.exports = app;
--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -16,12 +16,20 @@ const verifyToken = (req, res, next) => {
     }
 };
 
-// 관리자 권한 확인 미들웨어
-const isAdmin = (req, res, next) => {
-    if (req.user.role !== 'admin') {
-        return res.status(403).json({ message: 'Access denied' });
+// JWT 토큰 검증 미들웨어 (쿠키 사용)
+const verifyTokenCookie = (req, res, next) => {
+    const token = req.cookies.token; // 쿠키에서 토큰 추출
+    if (!token) {
+        return res.status(401).render('login', { message: '로그인이 필요합니다.' }); // 로그인 페이지로 리다이렉트
     }
-    next();
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded; // JWT에서 사용자 정보 추출
+        next(); // 다음 미들웨어로 전달
+    } catch (error) {
+        return res.status(403).render('login', { message: '유효하지 않은 토큰입니다. 다시 로그인해주세요.' }); // 로그인 페이지로 리다이렉트
+    }
 };
 
-module.exports = { verifyToken, isAdmin };
+module.exports = { verifyToken, verifyTokenCookie };
