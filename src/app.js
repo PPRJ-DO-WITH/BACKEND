22b9a38759fec477be8f3bddc2c644d6f13ed88f
SeolA-Lee <dlcodus1201@naver.com>
2024-12-13 03:56:26 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ const cookieParser = require('cookie-parser');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// const userRoutes = require('./routes/userRoutes');
-// const todoRoutes = require('./routes/todoRoutes');
-// const categRoutes = require('./routes/categRoutes');
-// const chatRoutes = require('./routes/chatRoutes');
-// const qnaRoutes = require('./routes/qnaRoutes');
+const userRoutes = require('./routes/userRoutes');
+const todoRoutes = require('./routes/todoRoutes');
+const categRoutes = require('./routes/categRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const qnaRoutes = require('./routes/qnaRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 /* 미들웨어 설정 */
@@ -23,11 +23,11 @@ app.use(express.static(__dirname + '/public'));
 app.use(cookieParser());
 
 /* 라우트 연결 */
-// app.use('/user', userRoutes);
-// app.use('/todo', todoRoutes);
-// app.use('/categ', categRoutes);
-// app.use('/chat', chatRoutes);
-// app.use('/qna', qnaRoutes);
+app.use('/user', userRoutes);
+app.use('/todo', todoRoutes);
+app.use('/categ', categRoutes);
+app.use('/chat', chatRoutes);
+app.use('/qna', qnaRoutes);
 app.use('/admin', adminRoutes);
 
 module.exports = app;
